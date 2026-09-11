80c7fddc6b2a0e1677a0fea8ef77f7742f2cda84
maxmunoz-debug <max.munoz@tecsup.edu.pe>
2026-09-11 01:06:35 -0500

    feat(sprint-1): Controlador inicial creado por Responsable

---
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -0,0 +1,7 @@
+package pe.edu.tecsup.lab03.controllers;
+
+public class StudentController {
+    public void showMessage() {
+        System.out.println("Versión inicial creada por Responsable");
+    }
+}
